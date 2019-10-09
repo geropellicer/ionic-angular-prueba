49444bf34d5926cf7f6f9d687ab21c01fa54eaef
Gero <geronimo@devsar.com>
2019-10-09 09:49:22 -0300

    Agrego gastos mock para tener para comenzar

---
+++ src/app/mock-gastos.ts
@@ -0,0 +1,16 @@
+import { Gasto } from './gasto';
+
+export const GASTOS: Gasto[] = [
+    { id: 1, gasto: 'Billetera virtual', precio: 120},
+    { id: 2, gasto: 'Chicles pegajosos', precio: 140},
+    { id: 3, gasto: 'Nuevo scrum master', precio: 20},
+    { id: 4, gasto: 'Derrota digna de los pumas', precio: 99},
+    { id: 5, gasto: 'Membresía SPOTIFY premium platinum', precio: 125},
+    { id: 6, gasto: 'Ganas de laburar', precio: 987},
+    { id: 7, gasto: 'Remedios para la abuela', precio: 754},
+    { id: 8, gasto: 'Saco pa tu mana', precio: 350},
+    { id: 9, gasto: 'Computadora nueva para codear mejor', precio: 10},
+    { id: 10, gasto: 'Título universitario', precio: 2350},
+    { id: 11, gasto: 'Ganas de pasarla bien', precio: 96},
+    { id: 12, gasto: 'Almuerzo vianda lunes', precio: 135},
+];
